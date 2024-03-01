5f09bf744f21989f7011d7a5b33510a533f9ec52
Kbenjamin92 <kipplbenjamin@gmail.com>
2024-03-01 15:15:20 -0500

    Refactored Game query

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,14 @@ import { Genre } from "./hooks/useGenres";
 import { PlatformSelector } from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 
+export interface GameQuery {
+  genre: Genre | null;
+  platform: Platform | null;
+}
+
 function App() {
-  const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
-  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(null);
+  const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+
   return (
     <>
       <Grid templateAreas={{
@@ -27,17 +32,15 @@ function App() {
         <Show above="lg">
           <GridItem area='aside' paddingX='15px'>
             <GenreList 
-              selectedGenre={selectedGenre} 
-              onSelectGenre={(genre) => setSelectedGenre(genre)}/>
+              selectedGenre={gameQuery.genre} 
+              onSelectGenre={(genre) => setGameQuery({...gameQuery, genre})}/>
           </GridItem>
         </Show>
         <GridItem area='main'>
           <PlatformSelector 
-            selectedPlatform={selectedPlatform} 
-            onSelectPlatform={(platform) => setSelectedPlatform(platform)}/>
-          <GameGrid 
-            selectedGenre={selectedGenre} 
-            selectedPlatform={selectedPlatform}/>
+            selectedPlatform={gameQuery.platform} 
+            onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})}/>
+          <GameGrid gameQuery={gameQuery} />
         </GridItem>
       </Grid>
     </>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,17 +1,16 @@
-import { Platform, useGames } from '../hooks/useGames';
+import { useGames } from '../hooks/useGames';
 import { GameCard } from './GameCard';
 import { Text, SimpleGrid } from '@chakra-ui/react';
 import { GameCardSkeleton } from './GameCardSkeleton';
 import { GameCardContainer } from './GameCardContainer';
-import { Genre } from '../hooks/useGenres';
+import { GameQuery } from '../App';
 
 interface Props {
-  selectedGenre: Genre | null;
-  selectedPlatform: Platform | null;
+  gameQuery: GameQuery;
 }
 
-export const GameGrid = ({ selectedGenre, selectedPlatform }: Props) => {
-    const { data, error, isLoading } = useGames(selectedGenre, selectedPlatform);
+export const GameGrid = ({ gameQuery }: Props) => {
+    const { data, error, isLoading } = useGames(gameQuery);
 
     const skeleton = [1, 2, 3, 4, 5, 6, 8, 10, 12, 14, 16, 18]
 
