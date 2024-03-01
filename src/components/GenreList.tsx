32474f6053a34b4ea78a516bf46a28f616db72ef
Kbenjamin92 <kipplbenjamin@gmail.com>
2024-02-29 22:53:23 -0500

    Fetch list of genres

---
+++ src/components/GenreList.tsx
@@ -0,0 +1,11 @@
+import useGenres from '../hooks/useGenres';
+
+export const GenreList = () => {
+    const { genres } = useGenres();
+
+  return (
+    <ul>
+      {genres.map(g => <li key={g.id}>{g.name}</li>)}
+    </ul>
+  )
+}
